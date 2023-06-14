1a134af5ae5ead18c182b1aff0aebbeba12233fa
Kishor Mahmud <dev.kishor138@gmail.com>
2023-06-15 03:22:43 +0600

    About me sevtion design

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,84 @@
 import React from "react";
+// import count up
+import CountUp from "react-countup";
+// import intersection observer hook
+import { useInView } from "react-intersection-observer";
+// import motion
+import { motion } from "framer-motion";
+// import variants
+import { fadeIn } from "../variants";
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
   return (
-    <div className="section" id="about">
-      About
-    </div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          {/* image  */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="lg:flex-1 lg:bg-about lg:bg-contain lg:bg-no-repeat lg:h-[540px] lg:mix-blend-lighten lg:bg-top"
+          ></motion.div>
+          {/* text  */}
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About me</h2>
+            <h3 className="h3 mb-4">
+              I am a Full Stack Developer with 1 year Experience.
+            </h3>
+            <p className="mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+              nisi sequi, placeat voluptas distinctio tempore corrupti quaerat
+              ipsum laborum excepturi?
+            </p>
+            {/* status  */}
+            <div className="flex gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br /> Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={30} duration={3} /> : null}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br /> Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={10} duration={3} /> : null}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Satisfy <br /> Client
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -34,7 +34,7 @@ const Banner = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+              className="mb-6 text-[36px] lg:text-[50px] font-secondary font-semibold uppercase leading-[1]"
             >
               <span className="text-white mr-4">I am a </span>
               <TypeAnimation
